c90466c3f08dadb25687071e865ab222098775ac
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2023-07-17 09:52:48 -0300

    configurando Stitches e estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { styled } from "@/styles"
+
+const Button = styled('button', {
+  backgroundColor: '$green500',
+})
+
 export default function Home() {
   return (
     <>
-      <h1>Teste</h1>
+      <Button>Teste</Button>
     </>
   )
 }
