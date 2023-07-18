e5d82ab41d8776c855b35fd33fe46cb10ab144cd
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2023-07-18 16:55:44 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,25 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
+import { useRouter } from "next/router";
+
 export default function Product() {
+  const { query } = useRouter();
+
   return (
     <>
-      <h1>Product</h1>
+      <ProductContainer>
+        <ImageContainer>
+        </ImageContainer>
+
+        <ProductDetails>
+          <h1>Camiseta 01</h1>
+          <span>R$ 79,90</span>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa fugiat ea perferendis ipsam earum voluptates laboriosam quidem optio. Impedit quisquam earum velit veniam ipsa provident odit, recusandae obcaecati tempore? Pariatur.</p>
+          <button>
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
     </>
   )
 }
