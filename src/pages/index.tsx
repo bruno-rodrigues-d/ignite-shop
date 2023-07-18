7137b5416afd9c400acf0a30da036c815f488d50
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2023-07-18 16:29:21 -0300

    Adicionando Link nos produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import 'keen-slider/keen-slider.min.css'
 import { GetStaticProps } from "next"
 
 import Stripe from 'stripe'
+import Link from "next/link"
 
 interface HomeProps {
   products: {
@@ -31,14 +32,18 @@ export default function Home({products}: HomeProps) {
       <HomeContainer ref={sliderRef} className="keen-slider">
         {products.map((product) => {
           return(
-            <Product className="keen-slider__slide" key={product.id}>
-              <Image src={product.imageUrl} width={520} height={480} alt='' />
+            <Link key={product.id} href={`/product/${product.id}`}>
+              <Product
+                className="keen-slider__slide"
+              >
+                <Image src={product.imageUrl} width={520} height={480} alt='' />
 
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
           )
           })
         }
