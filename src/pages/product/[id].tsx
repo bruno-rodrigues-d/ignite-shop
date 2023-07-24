736abd94b9d677b53c5f7ae3128dc6050c0054af
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2023-07-24 10:17:14 -0300

    feat: adding SEO

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import { useRouter } from "next/router";
 import axios from "axios";
 import { useState } from "react";
+import Head from "next/head";
 
 interface ProductProps {
   product: {
@@ -45,6 +46,10 @@ export default function Product({ product }: ProductProps) {
 
   return (
     <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
+
       <ProductContainer>
         <ImageContainer>
           <Image src={product.imageUrl} width={520} height={480} alt='' />
